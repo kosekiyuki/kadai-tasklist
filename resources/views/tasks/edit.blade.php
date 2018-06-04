96af6d816aea0178d92bc3e87d266cca269bc0fb
yuki.koseki <yuki.a.koseki@rakuten.com>
2018-06-04 01:15:30 +0000

    task ver 3.1

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,6 +4,8 @@
 <h1>精神と時の部屋</h1>
 <h2>id: {{ $task->id }} の修行編集ページ</h2>
 
+<div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+    
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
         {!! Form::label('status', '名前（種族）:') !!}
@@ -15,5 +17,7 @@
         {!! Form::submit('更新') !!}
 
     {!! Form::close() !!}
+
+</div>
 
 @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,6 +3,8 @@
 @section('content')
 <h1>精神と時の部屋</h1>
 <h2>id = {{ $task->id }} の修行詳細ページ</h2>
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+        
     <table class="table table-bordered">
         <tr>
             <th>id</th>
@@ -23,5 +25,6 @@
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
         {!! Form::submit('この修行をやめる', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
- 
+    </div>
+    
 @endsection
