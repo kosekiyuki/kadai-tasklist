c05f3df3ee1f09e6e3984830303fc430537f74bb
yuki.koseki <yuki.a.koseki@rakuten.com>
2018-06-12 05:19:47 +0000

    unnecessary

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', 'TasksController@index');
+// Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
 
